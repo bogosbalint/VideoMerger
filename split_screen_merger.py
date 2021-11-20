b69998d06b3036566db6c610e7d74ebca8b7b19f
bogosbalint <bogosbalint2001@gmail.com>
2021-11-20 20:08:46 +0100

    Módosítottam a split_screen_merger.py fájlt Feltöltések számának és a fáljok kiterjesztésének az ellenörzése, valamint a fájlok nevének a lerövidítése

--- split_screen_merger.py
+++ split_screen_merger.py
@@ -2,6 +2,7 @@ from moviepy.editor import *
 import tkinter as tk
 import tkinter.filedialog
 
+
 class GUI(tk.Frame):
 
     def __init__(self, master):
@@ -10,6 +11,7 @@ class GUI(tk.Frame):
         self.gui = self.configure_gui()
         self.widgets = self.create_widgets()
         self.imported_obj = []
+        self.count = 0
 
     def configure_gui(self):
         self.master.geometry('600x600')
@@ -19,14 +21,28 @@ class GUI(tk.Frame):
         import_btn.grid(sticky="W", column=0, row=0, padx=10)
 
     def import_file(self):
-        self.master.filename = tk.filedialog.askopenfilename(defaultextension=".mp4", filetypes=[("All files", "*.*")])
-        imported_video_label = tk.Label(self.master, justify=tk.LEFT, text=self.master.filename, font=(10))
-        imported_video_label.grid(sticky="W", column=0, row=len(self.imported_obj) + 5, padx=10)
-        self.imported_obj.append(self.master.filename)
+        if self.count != 4:
+            self.master.filename = tk.filedialog.askopenfilename(defaultextension=".mp4",
+                                                                 filetypes=[("All files", "*.*")])
+            file_name = ''
+            if self.master.filename.endswith('.mp4'):
+                length = len(self.master.filename) - 1
+                while length != 0:
+                    # print(length)
+                    if self.master.filename[length] == '/':
+                        file_name = self.master.filename[length + 1:]
+                        break
+                    length = length - 1
+                imported_video_label = tk.Label(self.master, justify=tk.LEFT, text=file_name, font=(10))
+                imported_video_label.grid(sticky="W", column=0, row=len(self.imported_obj) + 5, padx=10)
+                self.imported_obj.append(self.master.filename)
+
+                self.count = self.count + 1
+
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Big Python Project")
 
-    main_app =GUI(root)
+    main_app = GUI(root)
     root.mainloop()
