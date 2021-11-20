82e985069d31baa8e56d360f61efebf62644940a
bogosbalint <bogosbalint2001@gmail.com>
2021-11-20 19:23:53 +0100

    Módosítottam a split_screen_merger.py fájlt Fájlfeltöltéshez szükséges gomb és metódus létrehozása

--- split_screen_merger.py
+++ split_screen_merger.py
@@ -14,7 +14,11 @@ class GUI(tk.Frame):
         self.master.geometry('600x600')
 
     def create_widgets(self):
-        print('widget')
+        import_btn = tk.Button(self.master, text="Import", font=(12), command=self.import_file)
+        import_btn.grid(sticky="W", column=0, row=0, padx=10)
+
+    def import_file(self):
+        print('import')
 
 if __name__ == '__main__':
     root = tk.Tk()
