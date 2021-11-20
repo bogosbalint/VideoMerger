116da0391e517db9676b35ddd6dca5a6a3864649
Csépányi Gergely <csepanyigergely@gmail.com>
2021-11-20 18:58:00 +0100

    Módosítottam a split_screen_merger.py fájlt
    Implementáltam a program ablakának megjelenítésére szolgáló metódusokat és felhelyeztem a nézeteket.

--- split_screen_merger.py
+++ split_screen_merger.py
@@ -9,3 +9,16 @@ class GUI(tk.Frame):
         tk.Frame.__init__(self, self.master)
         self.gui = self.configure_gui()
         self.widgets = self.create_widgets()
+
+    def configure_gui(self):
+        self.master.geometry('600x600')
+
+    def create_widgets(self):
+        print('widget')
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Big Python Project")
+
+    main_app =GUI(root)
+    root.mainloop()
